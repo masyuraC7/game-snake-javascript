5318c4c868c47a0c3199ac9a86d3d64ff40faa72
masyuraC7 <masyurafanni1@gmail.com>
2024-10-02 20:17:50 +0700

    Change Snake Board Theme

--- snake.js
+++ snake.js
@@ -1,38 +1,39 @@
-var cvs = document.getElementById('snake');
-var ctx = cvs.getContext('2d');
+let game;
+
+var cvs = document.getElementById("snake");
+var ctx = cvs.getContext("2d");
+var snake_score = document.getElementById("score");
+var snake_time = document.getElementById("time");
 
 // ukuran box
 const box = 30;
 
-// memuat background
-let bg = new Image();
-bg.src = "bg-snake.png";
-
 // membuat kepala ular
 let snake = [];
 snake[0] = {
-	x:9 * box,
-	y:10 * box
+  x: 9 * box,
+  y: 10 * box,
 };
 
 // membuat makanan
 let food = {
-	x: Math.floor(Math.random()*18+1) * box,
-	y: Math.floor(Math.random()*16+3) * box
+  x: Math.floor(Math.random() * 39 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
 };
 
 // atur keyboard control
 document.addEventListener("keydown", direction);
 var alur;
 var waktu = 60;
+snake_time.innerHTML = waktu;
 
 function direction(e) {
-	var key = e.keyCode;
+  var key = e.keyCode;
 
-	if (key == 37 && alur != "RIGHT") alur = "LEFT";
-	else if (key == 38 && alur != "DOWN") alur = "UP";
-	else if (key == 39 && alur != "LEFT") alur = "RIGHT";
-	else if (key == 40 && alur != "UP") alur = "DOWN";
+  if (key == 37 && alur != "RIGHT") alur = "LEFT";
+  else if (key == 38 && alur != "DOWN") alur = "UP";
+  else if (key == 39 && alur != "LEFT") alur = "RIGHT";
+  else if (key == 40 && alur != "UP") alur = "DOWN";
 }
 
 // nilai awal
@@ -40,116 +41,122 @@ var nilai = 0;
 
 // cek saat tabrakan dengan ekor
 function tabrakan(head, body) {
-	for (var i = 0; i < body.length; i++) {
-		if (head.x == body[i].x && head.y == body[i].y) return true;
-	}
+  for (var i = 0; i < body.length; i++) {
+    if (head.x == body[i].x && head.y == body[i].y) return true;
+  }
 
-	return false;
+  return false;
 }
 
 function draw() {
-	// menggambar background
-	ctx.drawImage(bg,0,0);
+  // menggambar background
+  const cellSize = 30;
+  for (let i = 0; i < 40; i++) {
+    for (let j = 0; j < 16; j++) {
+      const color = (i + j) % 2 === 0 ? "#36c2ce" : "white";
+      ctx.fillStyle = color;
+      ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+    }
+  }
 
-	for (var i = 0; i < snake.length; i++) {
-		// warna ular
-		ctx.fillStyle = (i == 0) ? "#087716" : "white";
-		ctx.fillRect(snake[i].x,snake[i].y,box,box);
+  for (var i = 0; i < snake.length; i++) {
+    // warna ular
+    ctx.fillStyle = i == 0 ? "#478CCF" : "white";
+    ctx.fillRect(snake[i].x, snake[i].y, box, box);
 
-		// warna garis ular
-		ctx.strokeStyle = "black";
-		ctx.strokeRect(snake[i].x,snake[i].y,box,box);
-	}
+    // warna garis ular
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+  }
 
-	// warna makanan
-	ctx.fillStyle = "#ff9800";
-	ctx.fillRect(food.x,food.y,box,box);
-	ctx.strokeStyle = "black";
-	ctx.strokeRect(food.x,food.y,box,box);
+  // warna makanan
+  ctx.fillStyle = "#ff9800";
+  ctx.fillRect(food.x, food.y, box, box);
+  ctx.strokeStyle = "black";
+  ctx.strokeRect(food.x, food.y, box, box);
 
-	// kepala ular lama
-	let snakeX = snake[0].x;
-	let snakeY = snake[0].y;
+  // kepala ular lama
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
 
-	// atur gerakan ular
-	if (alur == "LEFT") snakeX -= box;
-	else if (alur == "UP") snakeY -= box;
-	else if (alur == "RIGHT") snakeX += box;
-	else if (alur == "DOWN") snakeY += box;
+  // atur gerakan ular
+  if (alur == "LEFT") snakeX -= box;
+  else if (alur == "UP") snakeY -= box;
+  else if (alur == "RIGHT") snakeX += box;
+  else if (alur == "DOWN") snakeY += box;
 
-	// saat menabrak makanan
-	if (snakeX == food.x && snakeY == food.y) {
-		// nilai bertambah
-		nilai++;
+  // saat menabrak makanan
+  if (snakeX == food.x && snakeY == food.y) {
+    // nilai bertambah
+    nilai++;
+    snake_score.innerHTML = nilai;
 
-		// membuat makanan baru
-		food = {
-			x: Math.floor(Math.random()*18+1) * box,
-			y: Math.floor(Math.random()*16+3) * box
-		};
-	}else {
-		snake.pop();
-	}
+    // membuat makanan baru
+    food = {
+      x: Math.floor(Math.random() * 39 + 1) * box,
+      y: Math.floor(Math.random() * 15 + 1) * box,
+    };
+  } else {
+    snake.pop();
+  }
 
-	let newHead = {
-		x : snakeX,
-		y : snakeY
-	};
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
 
-	// saat menabrak batas & ekor
-	if (snakeX > 18*box || snakeX < 1*box || snakeY < 3*box || snakeY > 18*box || tabrakan(newHead,snake)) {
-		// menghentikan game
-		clearInterval(game);
+  // saat menabrak batas & ekor
+  if (
+    snakeX > 39 * box ||
+    snakeX < 0 * box ||
+    snakeY < 0 * box ||
+    snakeY > 15 * box ||
+    tabrakan(newHead, snake)
+  ) {
+    // menghentikan game
+    clearInterval(game);
 
-		setTimeout(notif, 100);
-	}
+    setTimeout(notif, 100);
+  }
 
-	snake.unshift(newHead);
+  snake.unshift(newHead);
 
-	// cek waktu habis
-	if (waktu == 0) {
-		clearInterval(game);
+  // cek waktu habis
+  if (waktu == 0) {
+    clearInterval(game);
 
-		setTimeout(notif, 100);
-	}
-
-	// membuat tulisan nilai
-	ctx.fillStyle = "white";
-	ctx.fillText("Nilai : "+nilai, 1*box, 1*box+10);
-	ctx.font = "30px Arial white";
-
-	// membuat tulisan waktu
-	ctx.fillStyle = "white";
-	ctx.fillText("Waktu : "+waktu, 8*box-10, 1*box+10);
-	ctx.font = "30px Arial white";
-
-	// membuat tulisan game snake
-	ctx.fillStyle = "white";
-	ctx.fillText("Game Snake", 14*box, 1*box+10);
-	ctx.font = "30px Arial white";
+    setTimeout(notif, 100);
+  }
 }
 
 // fungsi restart
 function reload_game() {
-	location.reload();
+  location.reload();
+
+  document.getElementById("btn-start").style.display = "block";
 }
 
-// hitung mundur
-setTimeout(time, 1000);
+const snake_timeout = setTimeout(time, 1000);
+
+function start_game() {
+  game = setInterval(draw, 100);
+
+  document.getElementById("btn-start").style.display = "none";
+  document.getElementById("btn-restart").style.display = "block";
+}
 
 // fungsi hitung mundur
 function time() {
-	setTimeout(time, 1000);
-	waktu--;
-	if (waktu < 0) {
-		waktu = 0;
-		clearTimeout(time);
-	}
+  setTimeout(time, 1000);
+  waktu--;
+  snake_time.innerHTML = waktu;
+  if (waktu < 0) {
+    waktu = 0;
+    clearTimeout(time);
+  }
 }
 
 // pesan berakhir
 function notif(val) {
-	alert("Game over!!!\n"+"Nilai : "+nilai+"\nSisa waktu : "+waktu);
+  alert("Game over!!!\n" + "Nilai : " + nilai + "\nSisa waktu : " + waktu);
 }
-
-let game = setInterval(draw, 100);
